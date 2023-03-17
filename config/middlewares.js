38566123362998260f79f171482a17e32452e374
lawrence-dev21 <77075825+lawrence-dev21@users.noreply.github.com>
2023-03-17 03:08:01 +0200

    added upload module

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'pdf-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
